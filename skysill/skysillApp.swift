4d5f2a9982552c350cb775b3f998e127f08e26b0
expensivecake290 <107079460+expensivecake290@users.noreply.github.com>
2024-02-05 00:50:24 -0500

    Initial Commit

---
+++ skysill/skysillApp.swift
@@ -0,0 +1,17 @@
+//
+//  skysillApp.swift
+//  skysill
+//
+//  Created by Anthony garcia on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct skysillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
